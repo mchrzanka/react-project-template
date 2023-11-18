3ddacd3f2f80afeaee1149b46d70af1962aafb25
Maya C <70867990+mchrzanka@users.noreply.github.com>
2023-11-17 17:36:58 -0800

    navbar fixed

--- react-base/src/components/Navbar.js
+++ react-base/src/components/Navbar.js
@@ -14,45 +14,49 @@ function Navbar(props) {
 
 	return (
 		<>
-			{/* props.classname on navbar if we want to add any specific styling to the navbar on a certain page. */}
-			<nav className={`navbar ${props.className}`} role='navigation'>
-				<div className='logo'>
-					<a href='/'>
-						<img src={Logo} alt='Placeholder logo image'></img>
-					</a>
-				</div>
-				<div>
-					<ul className={click ? 'nav-menu active' : 'nav-menu'}>
-						<li className='nav-item'>
-							<Link to='/' className='nav-links' onClick={closeMobileMenu}>
-								Home
-							</Link>
-						</li>
-						<li className='nav-item'>
-							<Link to='/blog' className='nav-links' onClick={closeMobileMenu}>
-								Blog
-							</Link>
-						</li>
-						<li className='nav-item'>
-							<Link to='/about' onClick={closeMobileMenu}>
-								About Us
-							</Link>
-						</li>
-						<li className='nav-item'>
-							<Link to='/contact' onClick={closeMobileMenu}>
-								Contact Us
-							</Link>
-						</li>
-					</ul>
-				</div>
-				<div
-					onClick={handleClick}
-					aria-label='Main Menu'
-					className={click ? 'hamburger is-active' : 'hamburger'}
-				>
-					<span className='line'></span>
-					<span className='line'></span>
-					<span className='line'></span>
+			<nav className='navbar' role='navigation'>
+				<div className='navbar-container'>
+					<div className='logo'>
+						<a href='/'>
+							<img src={Logo} alt='Placeholder logo image'></img>
+						</a>
+					</div>
+					<div>
+						<ul className={click ? 'nav-menu active' : 'nav-menu'}>
+							<li className='nav-item'>
+								<Link to='/' className='nav-links' onClick={closeMobileMenu}>
+									Home
+								</Link>
+							</li>
+							<li className='nav-item'>
+								<Link
+									to='/about'
+									className='nav-links'
+									onClick={closeMobileMenu}
+								>
+									About
+								</Link>
+							</li>
+							<li className='nav-item'>
+								<Link
+									to='/contact'
+									className='nav-links'
+									onClick={closeMobileMenu}
+								>
+									Contact
+								</Link>
+							</li>
+						</ul>
+					</div>
+					<div
+						onClick={handleClick}
+						aria-label='Main Menu'
+						className={click ? 'hamburger is-active' : 'hamburger'}
+					>
+						<span className='line'></span>
+						<span className='line'></span>
+						<span className='line'></span>
+					</div>
 				</div>
 			</nav>
 		</>
